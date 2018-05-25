85308868bfc929a4c6c0eee1412c4be180728ba0
Badurov Kirill <wwwkom780@gmail.com>
2018-05-25 21:15:16 +0500

    Update bot.go

--- js/bot.go
+++ js/bot.go
@@ -7,6 +7,9 @@ import (
 func main() {
 	groupId := int32(166878888) // id сообщества
 	accessToken := "1ba480f143d3243fba526c88a64a7a650e422061352e74adb1e0c91cf374458f266b1c4a4fd3b3322f065" // Ключ доступа
+	
+	//Строка для подтверждения адреса сервера из настроек Callback API
+        $confirmationToken = '7cf2a4e1';
 
 	/* Создаем контекст бота */
 	bot := Organs.CreateBot(groupId, accessToken)
