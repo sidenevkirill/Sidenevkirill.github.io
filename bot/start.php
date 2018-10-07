ff8e07a8907ddabf4d219c97ee0403279a7d5fcd
Сиденёв Кирилл <wwwkom780@gmail.com>
2018-10-07 15:54:06 +0500

    Create start.php

---
+++ bot/start.php
@@ -0,0 +1,11 @@
+<?php
+require_once('core.php');
+$settings = (object) [
+ 'confirmation_token' => 'd0c2df8b', 
+ 'token' => 'e92e5fbaa553fb6fb8fd6c466b62046cdc9a45cf2fe0c00143d92ca16ee5f2168d0bd97e6dae96a34e4fd',
+ 'utoken' => 'e92e5fbaa553fb6fb8fd6c466b62046cdc9a45cf2fe0c00143d92ca16ee5f2168d0bd97e6dae96a34e4fd',
+ 'data' => json_decode(file_get_contents('php://input'))
+];
+$vk = new bot_vk($settings);
+$vk->start();
+?>
