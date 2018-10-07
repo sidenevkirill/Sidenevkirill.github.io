01888d68bf595ea4b443f9826416f03ee17b5a19
Сиденёв Кирилл <wwwkom780@gmail.com>
2018-10-07 15:37:46 +0500

    Create config.php

---
+++ www/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', '/var/www');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', 'd0c2df8b'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', 'e92e5fbaa553fb6fb8fd6c466b62046cdc9a45cf2fe0c00143d92ca16ee5f2168d0bd97e6dae96a34e4fd'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', 'e3caf7a0-60c9-4deb-baae-c59e34a141ec'); //Ключ для доступа к Yandex Speech Kit
