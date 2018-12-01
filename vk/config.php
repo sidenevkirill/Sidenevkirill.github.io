0a421fa0982ba789eaccb8eed67a1d7790bcfd87
Сиденёв Кирилл <wwwkom780@gmail.com>
2018-12-01 14:24:30 +0500

    Update config.php

--- vk/config.php
+++ vk/config.php
@@ -1,16 +1,16 @@
-$album = 'https://vk.com/album-113958919_252151167';
+$album = 'https://vk.com/album-157337613_252902419';
 $res = parse_url($album);
 $path = substr($res['path'], 6);
 $arr = explode('_', $path);
 $owner_id = $arr[0];
 $album_id = $arr[1];
 
-$standalone = "ваш standalone токен приложения";
-$group_token = 'ваш токен группы';
+$standalone = "4182274a27f6e4212f42408b857322f00fb3ff8d79fde8917cca747d6d572f310cafb9676f5c7ab6639f0";
+$group_token = '267b03db95bbbb8285d85c10ee3d3d1efbdeb163bad49782e93c0343a2965c276c4e637e36b90e02cc271';
 $conf = [
 	'standalone' => $standalone,
 	'group_token' => $group_token,
-	'contorm_token' => 'eae5d4a2',
+	'contorm_token' => '732be0d8',
 	'mess' => 'Фото в студио',
 	'not_command' => 'Ничего не понял!',
 	'owner_id' => $owner_id,
