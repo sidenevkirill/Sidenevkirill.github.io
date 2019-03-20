0775262bf48930ac07f495f08d43ad08d035b91f
Сиденёв Кирилл <wwwkom780@gmail.com>
2019-03-20 23:29:40 +0500

    Create danhsachbaihat.php

---
+++ song/danhsachbaihat.php
@@ -0,0 +1,7 @@
+
+Notice: Undefined variable: query in /storage/ssd2/134/3196134/public_html/Server/danhsachbaihat.php on line 41
+
+Warning: mysqli_query(): Empty query in /storage/ssd2/134/3196134/public_html/Server/danhsachbaihat.php on line 41
+
+Warning: mysqli_fetch_assoc() expects parameter 1 to be mysqli_result, boolean given in /storage/ssd2/134/3196134/public_html/Server/danhsachbaihat.php on line 42
+[]
