cec35e4838eee90bc8ec97f26754318fedd16813
Сиденёв Кирилл <wwwkom780@gmail.com>
2019-10-01 15:22:40 +0300

    Update hostmus.php

--- hostmus.php
+++ hostmus.php
@@ -1,12 +1,111 @@
 [
 
-{"id_song":"1",
+{
+"id_song":"1",
 "id_album":"1",
 "id_category":"1",
 "id_playlist":"2",
 "num_song":"1",
 "name_song":"Жак Энтони",
 "image_song":"https://sun9-3.userapi.com/c850724/v850724252/1beda4/DKaUjhJGIyY.jpg",
-"singer_song":"JAWS 2","link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Thay-La-Yeu-Thuong-OnlyC-Lou-Hoang.mp3"},
+"singer_song":"JAWS 2",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Thay-La-Yeu-Thuong-OnlyC-Lou-Hoang.mp3"},
 
-{"id_song":"41","id_album":"3","id_category":"3","id_playlist":"3","num_song":"2","name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em","image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg","singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n","link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"}]
+{
+"id_song":"2",
+"id_album":"2",
+"id_category":"2",
+"id_playlist":"2",
+"num_song":"2",
+"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"3",
+"id_album":"3",
+"id_category":"3",
+"id_playlist":"3",
+"num_song":"3",
+"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"4",
+"id_album":"4",
+"id_category":"4",
+"id_playlist":"4",
+"num_song":"4",
+"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"5",
+"id_album":"5",
+"id_category":"5",
+"id_playlist":"5",
+"num_song":"5",
+"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"6",
+"id_album":"6",
+"id_category":"6",
+"id_playlist":"6",
+"num_song":"6",
+"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"7",
+"id_album":"7",
+"id_category":"7",
+"id_playlist":"7",
+"num_song":"7",
+"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"8",
+"id_album":"8",
+"id_category":"8",
+"id_playlist":"8",
+"num_song":"8",
+"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"9",
+"id_album":"9",
+"id_category":"9",
+"id_playlist":"9",
+"num_song":"9",
+"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"10",
+"id_album":"10",
+"id_category":"10",
+"id_playlist":"10",
+"num_song":"10",
+"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"}]
