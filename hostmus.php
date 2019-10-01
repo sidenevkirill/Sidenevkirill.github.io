8c334bf3789be8242e464b1921131355953326f2
Сиденёв Кирилл <wwwkom780@gmail.com>
2019-10-01 15:13:09 +0300

    Update hostmus.php

--- hostmus.php
+++ hostmus.php
@@ -9,4 +9,4 @@
 "image_song":"https://sun9-3.userapi.com/c850724/v850724252/1beda4/DKaUjhJGIyY.jpg",
 "singer_song":"JAWS 2","link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Thay-La-Yeu-Thuong-OnlyC-Lou-Hoang.mp3"},
 
-{"id_song":"41","id_album":"3","id_category":"3","id_playlist":"3","numSong":"2","name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em","image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg","singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n","link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"}]
+{"id_song":"41","id_album":"3","id_category":"3","id_playlist":"3","num_song":"2","name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em","image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg","singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n","link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"}]
