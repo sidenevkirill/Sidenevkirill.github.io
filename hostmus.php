b358e445518cddc26bbb08955575b96da168543e
Сиденёв Кирилл <wwwkom780@gmail.com>
2019-10-11 00:32:55 +0300

    Update hostmus.php

--- hostmus.php
+++ hostmus.php
@@ -18,7 +18,7 @@
 "id_playlist":"2",
 "num_song":"2",
 "name_song":"Tanir, Tyomcha",
-"image_song":"https://hitmo.org/cover/song/66384585.jpg",
+"image_song":"https://cdn51.zvooq.com/pic?type=release&id=9324512&size=50x50&ext=jpg",
 "singer_song":"Da Da Da",
 "link_song":"https://hitmo.org/get/music/20190902/muzlome_Tanir_Tyomcha_-_Da_Da_Da_66384585.mp3"},
 
@@ -28,9 +28,9 @@
 "id_category":"3",
 "id_playlist":"3",
 "num_song":"3",
-"name_song":"Нурминский",
+"name_song":"JONY",
 "image_song":"https://hitmo.org/cover/song/66621751.jpg",
-"singer_song":"Валим",
+"singer_song":"Комета",
 "link_song":"https://hitmo.org/get/music/20190918/muzlome_Nurminskijj_-_Valim_66621751.mp3"},
 
 {
@@ -39,9 +39,9 @@
 "id_category":"4",
 "id_playlist":"4",
 "num_song":"4",
-"name_song":"Zivert",
+"name_song":"SAINt JHN",
 "image_song":"https://sun3-12.userapi.com/c858016/v858016225/6ae88/UbuaZVjuY1o.jpg",
-"singer_song":"Beverly Hills",
+"singer_song":"Roses",
 "link_song":"https://hitmo.org/get/music/20190906/muzlome_Zivert_-_Beverly_Hills_66445686.mp3"},
 
 {
@@ -50,9 +50,9 @@
 "id_category":"5",
 "id_playlist":"5",
 "num_song":"5",
-"name_song":"Tanir, Tyomcha",
+"name_song":"FACE",
 "image_song":"https://sun3-10.userapi.com/c851420/v851420866/18f657/ruhhDJnT4Ik.jpg",
-"singer_song":"Da Da Da",
+"singer_song":"СВЯЩЕННИК",
 "link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
 
 {
@@ -61,9 +61,9 @@
 "id_category":"6",
 "id_playlist":"6",
 "num_song":"6",
-"name_song":"NILETTO",
+"name_song":"Макс Корж",
 "image_song":"https://sun3-10.userapi.com/c851128/v851128188/108b41/lsB98vLQya4.jpg",
-"singer_song":"Любимка",
+"singer_song":"2 типа людей",
 "link_song":"https://hitmo.org/get/music/20190503/muzlome_NILETTO_-_Lyubimka_63911927.mp3"},
 
 {
@@ -72,9 +72,9 @@
 "id_category":"7",
 "id_playlist":"7",
 "num_song":"7",
-"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"name_song":"GAYAZOV$ BROTHER$",
 "image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
-"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"singer_song":"Увезите меня на Дип-хаус",
 "link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
 
 {
@@ -83,9 +83,9 @@
 "id_category":"8",
 "id_playlist":"8",
 "num_song":"8",
-"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"name_song":"FILV, Edmofo feat. Emma Peters",
 "image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
-"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"singer_song":"Clandestina",
 "link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
 
 {
@@ -94,9 +94,9 @@
 "id_category":"9",
 "id_playlist":"9",
 "num_song":"9",
-"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"name_song":"HammAli & Navai",
 "image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
-"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"singer_song":"Прятки",
 "link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
 
 {
@@ -105,7 +105,7 @@
 "id_category":"10",
 "id_playlist":"10",
 "num_song":"10",
-"name_song":"M\u1eb7t Tr\u1eddi C\u1ee7a Em",
+"name_song":"Shawn Mendes, Camila Cabello",
 "image_song":"https:\/\/unpeeled-toes.000webhostapp.com\/images\/songs\/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
-"singer_song":"Ph\u01b0\u01a1ng Ly , JustaTee\r\n",
+"singer_song":"Señorita",
 "link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"}]
