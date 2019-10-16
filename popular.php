bd5a1c76af348eded3c05615e754e0bb24fd721a
Сиденёв Кирилл <wwwkom780@gmail.com>
2019-10-16 18:56:02 +0300

    Create popular.php

---
+++ popular.php
@@ -0,0 +1,113 @@
+[
+
+{
+"id_song":"1",
+"id_album":"1",
+"id_category":"1",
+"id_playlist":"1",
+"name_song":"Zivert",
+"image_song":"https://cdn51.zvooq.com/pic?type=release&id=9350408&size=90x90&ext=jpg",
+"singer_song":"Pop",
+"link_song":"https://cs3-1v4.vkuseraudio.net/p22/dc0d9560c40fe7.mp3?extra=u3MQEDQ1aFU4GMgU6uBcEEra_s86eIkUgUf5-JeqXSS7MQ_VuGMryEHBk4b_ccHNIa5YulxkGs5l_lMeQTjwo6Zv2uqjfzFhXOf4vDZbVCM2TWxDgTO1JIOAATht2QHfML7f70gs57dppft9EBL6f7q0lg"},
+
+{
+"id_song":"2",
+"id_album":"2",
+"id_category":"2",
+"id_playlist":"2",
+"name_song":"Tanir, Tyomcha",
+"image_song":"https://cdn51.zvooq.com/pic?type=release&id=9324512&size=50x50&ext=jpg",
+"singer_song":"Da Da Da",
+"link_song":"https://hitmo.org/get/music/20190902/muzlome_Tanir_Tyomcha_-_Da_Da_Da_66384585.mp3"},
+
+{
+"id_song":"3",
+"id_album":"3",
+"id_category":"3",
+"id_playlist":"3",
+"name_song":"JONY",
+"image_song":"https://cdn62.zvooq.com/pic?type=release&id=9513393&size=50x50&ext=jpg",
+"singer_song":"Комета",
+"link_song":"https://hitmo.org/get/music/20190918/muzlome_Nurminskijj_-_Valim_66621751.mp3"},
+
+{
+"id_song":"4",
+"id_album":"4",
+"id_category":"4",
+"id_playlist":"4",
+"name_song":"SAINt JHN",
+"image_song":"https://cdn62.zvooq.com/pic?type=release&id=9492563&size=50x50&ext=jpg",
+"singer_song":"Roses",
+"link_song":"https://hitmo.org/get/music/20190906/muzlome_Zivert_-_Beverly_Hills_66445686.mp3"},
+
+{
+"id_song":"5",
+"id_album":"5",
+"id_category":"5",
+"id_playlist":"5",
+"name_song":"FACE",
+"image_song":"https://cdn61.zvooq.com/pic?type=release&id=9571400&size=50x50&ext=jpg",
+"singer_song":"СВЯЩЕННИК",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"6",
+"id_album":"6",
+"id_category":"6",
+"id_playlist":"6",
+"name_song":"Макс Корж",
+"image_song":"https://cdn52.zvooq.com/pic?type=release&id=9339261&size=50x50&ext=jpg",
+"singer_song":"2 типа людей",
+"link_song":"https://hitmo.org/get/music/20190503/muzlome_NILETTO_-_Lyubimka_63911927.mp3"},
+
+{
+"id_song":"7",
+"id_album":"7",
+"id_category":"7",
+"id_playlist":"7",
+"name_song":"GAYAZOV$ BROTHER$",
+"image_song":"https://cdn52.zvooq.com/pic?type=release&id=9620580&size=50x50&ext=jpg",
+"singer_song":"Увезите меня на Дип-хаус",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"8",
+"id_album":"8",
+"id_category":"8",
+"id_playlist":"8",
+"name_song":"FILV, Edmofo feat. Emma Peters",
+"image_song":"https://cdn41.zvooq.com/pic?type=release&id=9393827&size=50x50&ext=jpg",
+"singer_song":"Clandestina",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"9",
+"id_album":"9",
+"id_category":"9",
+"id_playlist":"9",
+"name_song":"HammAli & Navai",
+"image_song":"https://cdn51.zvooq.com/pic?type=release&id=8975751&size=50x50&ext=jpg",
+"singer_song":"Прятки",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"11",
+"id_album":"11",
+"id_category":"11",
+"id_playlist":"11",
+"name_song":"Bonsai",
+"image_song":"https://cdn52.zvooq.com/pic?type=release&id=9447718&size=50x50&ext=jpg",
+"singer_song":"Она",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
+
+{
+"id_song":"10",
+"id_album":"10",
+"id_category":"10",
+"id_playlist":"10",
+"name_song":"Shawn Mendes, Camila Cabello",
+"image_song":"https://cdn62.zvooq.com/pic?type=release&id=8896969&size=50x50&ext=jpg",
+"singer_song":"Señorita",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"}
+
+]
