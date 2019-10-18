e73c43742d0c5bc971c60ce1499e2ef2de8dd7a6
Сиденёв Кирилл <wwwkom780@gmail.com>
2019-10-18 17:02:02 +0300

    Update hostmus.php

--- hostmus.php
+++ hostmus.php
@@ -733,6 +733,12 @@
 "singer_song":"My Neck, My Back",
 "link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
 
+{
+"id_song":"100",
+"name_song":"Audiosoulz",
+"image_song":"https://cdn62.zvooq.com/pic?type=release&id=8842468&size=90x90&ext=jpg",
+"singer_song":"Dancefloor",
+"link_song":"https:\/\/unpeeled-toes.000webhostapp.com\/songs\/Mat-Troi-Cua-Em-Phuong-Ly-JustaTee.mp3"},
 
 {
 "id_song":"10",
