3cfd3e912ef6b62d978e1cea6db1853bddc4a19e
Сиденёв Кирилл <wwwkom780@gmail.com>
2020-08-10 20:52:48 +0500

    Create slider.php

---
+++ slider.php
@@ -0,0 +1,18 @@
+ [
+
+    {
+            "id_song":"1",
+            "nama":"Zivert",
+            "gambar":"Credo",
+            "value":"Credo",
+            "message":"Credo",
+            "result":"https://i1.sndcdn.com/artworks-000536328417-wg4psy-t500x500.jpg",
+
+    {
+            "id_song":"2",
+            "nama":"Zivert",
+            "gambar":"Credo",
+            "value":"Credo",
+            "message":"Credo",
+            "result":"https://i1.sndcdn.com/artworks-000536328417-wg4psy-t500x500.jpg",
+]
